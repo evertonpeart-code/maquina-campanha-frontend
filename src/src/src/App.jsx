e1b9586d1a10c95c19aad353cde4f37a0c321628
Everton <evertoncostapeart@gmail.com>
2025-08-07 22:16:45 -0300

    Delete src/src/src directory

--- src/src/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 text-2xl">
-      🚀 Máquina de Campanha IA - Frontend funcionando!
-    </div>
-  )
-}
