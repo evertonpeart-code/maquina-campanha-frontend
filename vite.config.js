dbc8bfec7cc0d5fff399836489893ae64e8d8374
Everton <evertoncostapeart@gmail.com>
2025-08-08 19:37:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()]
+  root: path.resolve(__dirname), // mantém a raiz do projeto
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['maquina-campanha-frontend.onrender.com']
+  }
 })
