d52de3b73b80d6aee0ed1df83dd26775b3b49b96
Everton <evertoncostapeart@gmail.com>
2025-08-10 20:38:52 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+// postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Adiciona suporte para minificação e otimização em produção
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          require('cssnano')({
+            preset: 'default',
+          }),
+        ]
+      : []),
+  ],
+};
